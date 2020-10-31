e7b0dc32c8eea5a01607631117726aca5b627ffc
skrobman <72264536+skrobman@users.noreply.github.com>
2020-10-31 13:43:00 +0300

    Add files via upload

---
+++ DZ 31.10.20/Square/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            if(j == i){
+                cout << 0;
+            }
+            else{
+                cout << 1;
+            }
+            cout << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
